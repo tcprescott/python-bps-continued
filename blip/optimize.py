fbfa3e536850d9ac3dc6b28569ab8a85a668ff97
Tim Allen <screwtape@froup.com>
2011-04-04 22:48:36 +1000

    Add blip-optimize tool for optimizing patches.
    Currently just does a few simple transformations that fix obvious
    brain-dead behaviours like having consecutive SourceRead or TargetRead
    operations.

---
+++ blip/optimize.py
@@ -0,0 +1,54 @@
+"""
+Tools for optimizing blip patches.
+"""
+from blip import constants as C
+from blip.validate import check_stream
+
+def optimize(iterable):
+	"""
+	Yields a simplified sequence of patch operations from iterable.
+	"""
+	iterable = check_stream(iterable)
+
+	header = next(iterable)
+	yield header
+
+	lastItem = next(iterable)
+
+	for item in iterable:
+
+		# FIXME: Another idea for optimizing: if we keep track of
+		# targetWriteOffset and sourceRelativeOffset in here, we convert
+		# SourceCopy operations with an appropriate offset into SourceRead
+		# operations, which should shave off a few bytes.
+
+		if lastItem[0] == C.SOURCEREAD and item[0] == C.SOURCEREAD:
+			# We can merge consecutive SourceRead operations.
+			lastItem = (C.SOURCEREAD, lastItem[1] + item[1])
+			continue
+
+		elif lastItem[0] == C.TARGETREAD and item[0] == C.TARGETREAD:
+			# We can merge consecutive TargetRead operations.
+			lastItem = (C.TARGETREAD, lastItem[1] + item[1])
+			continue
+
+		elif (lastItem[0] == C.SOURCECOPY and item[0] == C.SOURCECOPY and
+				item[2] == 0):
+			# We can merge consecutive SourceCopy operations, as long as the
+			# following ones have a relative offset of 0 from the end of the
+			# previous one.
+			lastItem = (C.SOURCECOPY, lastItem[1] + item[1], lastItem[2])
+			continue
+
+		elif (lastItem[0] == C.TARGETCOPY and item[0] == C.TARGETCOPY and
+				item[2] == 0):
+			# We can merge consecutive TargetCopy operations, as long as the
+			# following ones have a relative offset of 0 from the end of the
+			# previous one.
+			lastItem = (C.TARGETCOPY, lastItem[1] + item[1], lastItem[2])
+			continue
+
+		yield lastItem
+		lastItem = item
+
+	yield lastItem
---
+++ blip/test/test_optimize.py
@@ -0,0 +1,140 @@
+#!/usr/bin/python3
+import unittest
+from io import BytesIO
+from blip import constants as C
+from blip.io import read_blip
+from blip.test.util import find_blip
+from blip.validate import check_stream
+from blip.optimize import optimize
+
+
+def test_optimize(iterable):
+	"""
+	Check that the input to and output from optimize is sensible.
+	"""
+	for item in check_stream(optimize(check_stream(iterable))):
+		yield item
+
+
+class TestOptimize(unittest.TestCase):
+
+	def testEmptyPatch(self):
+		"""
+		The simplest possible patch can be processed correctly.
+		"""
+		original = list(read_blip(BytesIO(find_blip("empty"))))
+		result = list(test_optimize(original))
+
+		self.assertSequenceEqual(original, result)
+
+	def testMergeSourceReads(self):
+		"""
+		Consecutive SourceReads are merged.
+		"""
+		original = [
+				(C.BLIP_MAGIC, 3, 3, ""),
+				(C.SOURCEREAD, 1),
+				(C.SOURCEREAD, 1),
+				(C.SOURCEREAD, 1),
+				(C.SOURCECRC32, 0x66A031A7),
+				(C.TARGETCRC32, 0x66A031A7),
+			]
+
+		expected = [
+				(C.BLIP_MAGIC, 3, 3, ""),
+				(C.SOURCEREAD, 3),
+				(C.SOURCECRC32, 0x66A031A7),
+				(C.TARGETCRC32, 0x66A031A7),
+			]
+
+		actual = list(test_optimize(original))
+
+		self.assertSequenceEqual(expected, actual)
+
+	def testMergeTargetReads(self):
+		"""
+		Consecutive TargetReads are merged.
+		"""
+		original = [
+				(C.BLIP_MAGIC, 0, 3, ""),
+				(C.TARGETREAD, b'A'),
+				(C.TARGETREAD, b'A'),
+				(C.TARGETREAD, b'A'),
+				(C.SOURCECRC32, 0x00000000),
+				(C.TARGETCRC32, 0x66A031A7),
+			]
+
+		expected = [
+				(C.BLIP_MAGIC, 0, 3, ""),
+				(C.TARGETREAD, b'AAA'),
+				(C.SOURCECRC32, 0x00000000),
+				(C.TARGETCRC32, 0x66A031A7),
+			]
+
+		actual = list(test_optimize(original))
+
+		self.assertSequenceEqual(expected, actual)
+
+	def testMergeContiguousSourceCopys(self):
+		"""
+		A SourceCopy is merged if its offset is zero.
+		"""
+		original = [
+				(C.BLIP_MAGIC, 3, 3, ""),
+				(C.SOURCECOPY, 1, 0),
+				# This SourceCopy has an offset of 0, so it can be merged with
+				# the previous one.
+				(C.SOURCECOPY, 1, 0),
+				# This SourceCopy has an offset pointing somewhere else in the
+				# file, so it can't be merged.
+				(C.SOURCECOPY, 1, -2),
+				(C.SOURCECRC32, 0x66A031A7),
+				(C.TARGETCRC32, 0x66A031A7),
+			]
+
+		expected = [
+				(C.BLIP_MAGIC, 3, 3, ""),
+				(C.SOURCECOPY, 2, 0),
+				(C.SOURCECOPY, 1, -2),
+				(C.SOURCECRC32, 0x66A031A7),
+				(C.TARGETCRC32, 0x66A031A7),
+			]
+
+		actual = list(test_optimize(original))
+
+		self.assertSequenceEqual(expected, actual)
+
+	def testMergeContiguousTargetCopys(self):
+		"""
+		A TargetCopy is merged if its offset is zero.
+		"""
+		original = [
+				(C.BLIP_MAGIC, 0, 4, ""),
+				(C.TARGETREAD, b'A'),
+				(C.TARGETCOPY, 1, 0),
+				# This SourceCopy has an offset of 0, so it can be merged with
+				# the previous one.
+				(C.TARGETCOPY, 1, 0),
+				# This SourceCopy has an offset pointing somewhere else in the
+				# file, so it can't be merged.
+				(C.TARGETCOPY, 1, -2),
+				(C.SOURCECRC32, 0x00000000),
+				(C.TARGETCRC32, 0x66A031A7),
+			]
+
+		expected = [
+				(C.BLIP_MAGIC, 0, 4, ""),
+				(C.TARGETREAD, b'A'),
+				(C.TARGETCOPY, 2, 0),
+				(C.TARGETCOPY, 1, -2),
+				(C.SOURCECRC32, 0x00000000),
+				(C.TARGETCRC32, 0x66A031A7),
+			]
+
+		actual = list(test_optimize(original))
+
+		self.assertSequenceEqual(expected, actual)
+
+
+if __name__ == "__main__":
+	unittest.main()
