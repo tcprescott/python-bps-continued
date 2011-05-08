f8e701819b77fdf99f59e4c6db51c60348997b9a
Tim Allen <screwtape@froup.com>
2011-05-08 11:30:02 +1000

    Avoid O(n^2) concatenation when merging TargetReads.
    Python strings are immutable, so concatenating two strings results in
    allocation of a new, third string large enough to contain the other two.
    Because a TargetRead operation represents its payload as a string,
    merging large TargetRead operations is likely to experience
    a performance problem.
    
    Therefore, blip.optimize has been taught to merge consecutive
    TargetReads into a BytesIO object, flattening it into a single byte
    string right at the end. This improves runtime on one of my test
    cases from about two hours to two minutes.

--- blip/optimize.py
+++ blip/optimize.py
@@ -8,9 +8,18 @@
 """
 Tools for optimizing blip patches.
 """
+from io import BytesIO
 from blip import constants as C
 from blip.validate import check_stream
 from blip.util import op_size
+
+# It's semantically correct to concatentate two or more TargetRead operations
+# by concatenating their payloads, but repeated string concatenation can be
+# hideously inefficient in Python. Therefore, for the duration of this module,
+# we define a new operation type, "ElasticTargetRead", that is semantically
+# identical to TargetRead, but stores its payload in a BytesIO object rather
+# than in a raw bytes object.
+ELASTIC_TARGETREAD = "ElasticTargetRead"
 
 def optimize(iterable):
 	"""
@@ -37,8 +46,17 @@ def optimize(iterable):
 			continue
 
 		elif lastItem[0] == C.TARGETREAD and item[0] == C.TARGETREAD:
-			# We can merge consecutive TargetRead operations.
-			lastItem = (C.TARGETREAD, lastItem[1] + item[1])
+			# We can merge consecutive TargetRead operations. For efficiency,
+			# we'll merge them into an ElasticTargetRead operation.
+			buf = BytesIO()
+			buf.write(lastItem[1])
+			buf.write(item[1])
+			lastItem = (ELASTIC_TARGETREAD, buf)
+			continue
+
+		elif lastItem[0] == ELASTIC_TARGETREAD and item[0] == C.TARGETREAD:
+			# Another item to add to the growing TargetRead.
+			lastItem[1].write(item[1])
 			continue
 
 		elif (lastItem[0] == C.SOURCECOPY and item[0] == C.SOURCECOPY and
@@ -58,7 +76,14 @@ def optimize(iterable):
 			continue
 
 		if lastItem[0] == C.SOURCECOPY and lastItem[2] == targetWriteOffset:
+			# A SourceRead is just a SourceCopy that implicitly has its read
+			# off set set to targetWriteOffset.
 			lastItem = (C.SOURCEREAD, lastItem[1])
+
+		if lastItem[0] == ELASTIC_TARGETREAD:
+			# If we've come to the end of the chain of TargetRead operations,
+			# merge our buffer back into a single TargetRead operation.
+			lastItem = (C.TARGETREAD, lastItem[1].getvalue())
 
 		yield lastItem
 
@@ -67,5 +92,10 @@ def optimize(iterable):
 			targetWriteOffset += op_size(lastItem)
 
 		lastItem = item
+
+	if lastItem[0] == ELASTIC_TARGETREAD:
+		# If we've come to the end of the chain of TargetRead operations,
+		# merge our buffer back into a single TargetRead operation.
+		lastItem = (C.TARGETREAD, lastItem[1].getvalue())
 
 	yield lastItem
